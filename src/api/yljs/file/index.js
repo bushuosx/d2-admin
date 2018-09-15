2962db640c4c8730fe28d4d9e7a3f976354e35c0
bushuosx <bushuosx@live.com>
2018-09-15 13:11:01 +0800

    fastuplodad working 2

--- src/api/yljs/file/index.js
+++ src/api/yljs/file/index.js
@@ -1,20 +1,25 @@
-import axiosService from '@/plugin/axios'
-import parent from '../index'
+import axiosService from "@/plugin/axios";
+import parent from "../index";
 
-const BaseURL = parent.BaseURL + '/file'
+const BaseURL = parent.BaseURL + "/file";
 
 export default {
   BaseURL,
-  post ({ filename, filelength, filesha1 }) {
-    axiosService.post(BaseURL, { filename, filelength, filesha1 })
+  post({ filename, filelength, filesha1 }) {
+    axiosService.post(BaseURL, { filename, filelength, filesha1 });
   },
-  put ({ writetoken, position, file, length }) {
-    axiosService.put(BaseURL, { writetoken, position, file, length })
+  put({ writetoken, position, data, size }) {
+    var form = new FormData();
+    form.append(writeToken);
+    form.append(position);
+    form.append("file", data);
+    form.append("length", size);
+    axiosService.put(BaseURL, { writetoken, position, data, size });
   },
-  complite (writetoken) {
-    axiosService.get(BaseURL + '/' + writetoken)
+  complite(writetoken) {
+    axiosService.post(BaseURL + "/complite", { writetoken });
   },
-  get (fileid) {
-    axiosService.get(BaseURL + '/' + fileid)
+  get(fileid) {
+    axiosService.get(BaseURL + "/" + fileid);
   }
-}
+};
